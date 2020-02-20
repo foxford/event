ec5d3d4ce0d62ccea55d1b49ddd14707122ffdb5
Timofey Martynov <feymartynov@gmail.com>
2020-02-20 09:31:00 +0300

    Fix state calculation for forward direction

--- migrations/2020-02-17-201407_create_event_state_view/up.sql
+++ migrations/2020-02-17-201407_create_event_state_view/up.sql
@@ -1,8 +1,18 @@
-create view event_state as (
+-- We have to use views because diesel doesn't support subqueries.
+create view event_state_backward as (
   select distinct
     first_value(id) over (
       partition by room_id, set, label
       order by occurred_at desc, created_at desc
+    ) as id
+  from event
+);
+
+create view event_state_forward as (
+  select distinct
+    first_value(id) over (
+      partition by room_id, set, label
+      order by occurred_at, created_at
     ) as id
   from event
 );
