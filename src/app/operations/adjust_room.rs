71127b33cfbfe6f9ece9df21c2bf6d51456dc94d
Timofey Martynov <feymartynov@gmail.com>
2020-04-02 03:27:04 +0300

    Fix room.adjust corner case

--- src/app/operations/adjust_room.rs
+++ src/app/operations/adjust_room.rs
@@ -256,6 +256,11 @@ fn invert_segments(
     segments: &[Segment],
     room_duration: Duration,
 ) -> Result<Vec<(i64, i64)>, Error> {
+    if segments.len() == 0 {
+        let total_nanos = room_duration.num_nanoseconds().unwrap_or(std::i64::MAX);
+        return Ok(vec![(0, total_nanos)]);
+    }
+
     let mut gaps = Vec::with_capacity(segments.len() + 2);
 
     // A possible gap before the first segment.
