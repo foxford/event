8f8bf0d9ecfeca950402065e62e12fa3dd176c77
Ilya Titkov <ilya@titkov.me>
2021-08-11 17:20:16 +0300

    Remove unused param

--- src/app/operations/adjust_room.rs
+++ src/app/operations/adjust_room.rs
@@ -191,7 +191,7 @@ pub(crate) async fn call(
             )
         })?;
 
-    let cut_gaps = cut_events_to_gaps(room_duration, &cut_events)?;
+    let cut_gaps = cut_events_to_gaps(&cut_events)?;
 
     // Create modified room with events shifted again according to cut events this time.
     let modified_room = create_room(
@@ -420,10 +420,7 @@ enum CutEventsToGapsState {
 }
 
 /// Transforms cut-start/stop events ordered list to gaps list with a simple FSM.
-pub(crate) fn cut_events_to_gaps(
-    room_duration: Duration,
-    cut_events: &[Event],
-) -> Result<Vec<(i64, i64)>> {
+pub(crate) fn cut_events_to_gaps(cut_events: &[Event]) -> Result<Vec<(i64, i64)>> {
     let mut gaps = Vec::with_capacity(cut_events.len());
     let mut state: CutEventsToGapsState = CutEventsToGapsState::Started(0);
 
