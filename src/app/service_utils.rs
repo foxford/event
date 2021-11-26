b8881874cf9dcc6373bd2a77c1b62987ebe1de2d
Shamir Khodzha <khodzha.sh@gmail.com>
2021-11-26 16:34:04 +0300

    fixed streamed mqtt messages collision with vec of mqtt messages in http extensions

--- src/app/service_utils.rs
+++ src/app/service_utils.rs
@@ -13,18 +13,61 @@ use svc_agent::{
 };
 use tokio::task::JoinHandle;
 
-use crate::app::message_handler::MessageStream;
+use crate::app::message_handler::{Message, MessageStream, MessageStreamTrait};
 use crate::app::{endpoint::helpers, error::ErrorExt};
 
 use super::error;
 
+#[derive(Default)]
+pub struct Notifications(Vec<Message>);
+
+impl Notifications {
+    fn into_stream(self) -> impl MessageStreamTrait {
+        stream::iter(self.0)
+    }
+
+    fn push(&mut self, msg: Message) {
+        self.0.push(msg);
+    }
+}
+
+impl IntoIterator for Notifications {
+    type Item = <Vec<Message> as IntoIterator>::Item;
+    type IntoIter = <Vec<Message> as IntoIterator>::IntoIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
+
+#[derive(Default)]
+pub struct AsyncTasks(Vec<JoinHandle<Message>>);
+
+impl AsyncTasks {
+    fn into_stream(self) -> impl MessageStreamTrait {
+        stream::iter(self.0)
+            .flat_map(stream::once)
+            .filter(|jh_output| {
+                if let Err(e) = jh_output {
+                    error!(err = ?e, "Failed to await async task, join handle error");
+                }
+                future::ready(jh_output.is_ok())
+            })
+            .map(|jh_output| jh_output.unwrap())
+    }
+
+    fn push(&mut self, msg: JoinHandle<Message>) {
+        self.0.push(msg);
+    }
+}
+
 pub struct Response {
-    notifications: Vec<Box<dyn IntoPublishableMessage + Send + Sync + 'static>>,
+    notifications: Notifications,
     status: StatusCode,
     start_timestamp: DateTime<Utc>,
     authz_time: Option<Duration>,
     payload: Result<Value, serde_json::Error>,
-    async_tasks: Vec<JoinHandle<Box<dyn IntoPublishableMessage + Send + Sync + 'static>>>,
+    async_tasks: AsyncTasks,
 }
 
 impl Response {
@@ -35,12 +78,12 @@ impl Response {
         maybe_authz_time: Option<Duration>,
     ) -> Self {
         Self {
-            notifications: Vec::new(),
+            notifications: Default::default(),
             status,
             start_timestamp,
             authz_time: maybe_authz_time,
             payload: serde_json::to_value(&payload),
-            async_tasks: Vec::new(),
+            async_tasks: Default::default(),
         }
     }
 
@@ -61,12 +104,9 @@ impl Response {
             notifications.push(response);
         }
 
-        let tasks_stream = stream::iter(self.async_tasks)
-            .flat_map(stream::once)
-            .filter(|jh_output| future::ready(jh_output.is_ok()))
-            .map(|jh_output| jh_output.unwrap());
-
-        let stream = stream::iter(notifications).chain(tasks_stream);
+        let stream = notifications
+            .into_stream()
+            .chain(self.async_tasks.into_stream());
 
         Ok(Box::new(stream))
     }
@@ -81,6 +121,7 @@ impl Response {
         let timing = ShortTermTimingProperties::until_now(start_timestamp);
         let props = OutgoingEventProperties::new(label, timing);
         self.notifications
+            .0
             .push(Box::new(OutgoingEvent::broadcast(payload, props, path)))
     }
 
@@ -98,10 +139,12 @@ impl IntoResponse for Response {
     type BodyError = <Self::Body as axum::body::HttpBody>::Error;
 
     fn into_response(self) -> hyper::Response<Self::Body> {
+        let tasks_stream = Box::new(self.async_tasks.into_stream()) as MessageStream;
+
         http::Response::builder()
             .status(self.status)
             .extension(self.notifications)
-            .extension(self.async_tasks)
+            .extension(tasks_stream)
             .body(axum::body::Body::from(
                 serde_json::to_string(&self.payload.expect("Todo")).expect("todo"),
             ))
