48989db4a11feb8fc593d8951fb4188ff0180990
Mikhail Grachev <work@mgrachev.com>
2022-05-11 11:42:16 +0300

    Update dependencies

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,9 @@ const APP_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    #[cfg(feature = "dotenv")]
+    dotenv::dotenv()?;
+
     tracing_log::LogTracer::init()?;
 
     let (non_blocking, _guard) = tracing_appender::non_blocking(std::io::stdout());
