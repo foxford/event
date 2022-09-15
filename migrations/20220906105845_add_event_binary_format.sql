2d1d24c2000d525e45f75d049c498dd8588bf892
Dmitry Shlagoff <richyannet@yandex.ru>
2022-09-15 17:47:58 +0200

    Fix migrations a bit

--- migrations/20220906105845_add_event_binary_format.sql
+++ migrations/20220906105845_add_event_binary_format.sql
@@ -1,5 +1,5 @@
 -- add column for event binary repr
-ALTER TABLE event ADD COLUMN binary_data bytea;
+ALTER TABLE event ADD COLUMN IF NOT EXISTS binary_data bytea;
 -- allow data to be null w/o default but only
 -- if binary_data is present and vice versa
 ALTER TABLE event ALTER COLUMN data DROP NOT NULL;
@@ -7,7 +7,7 @@ ALTER TABLE event ALTER COLUMN data DROP DEFAULT;
 ALTER TABLE event ADD CONSTRAINT data_or_binary_data_not_null
     CHECK ((binary_data IS NOT NULL) OR (data IS NOT NULL)) NOT VALID;
 -- check data size for both data fields
-ALTER TABLE event DROP CONSTRAINT data_size;
+ALTER TABLE event DROP CONSTRAINT IF EXISTS data_size;
 ALTER TABLE event ADD CONSTRAINT data_size
     CHECK (deleted_at IS NOT NULL
     OR (COALESCE(octet_length(data::text), 0) + COALESCE(octet_length(binary_data::bytea), 0)) < 102400) NOT VALID;
