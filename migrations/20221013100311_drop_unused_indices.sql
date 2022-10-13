7591d6ab0ffb922a211dcb469124cedf9e9a9181
Dmitry Shlagoff <richyannet@yandex.ru>
2022-10-13 12:40:29 +0200

    Do not run concurrently b/c it doesn't work in tx

--- migrations/20221013100311_drop_unused_indices.sql
+++ migrations/20221013100311_drop_unused_indices.sql
@@ -1,2 +1,2 @@
-DROP INDEX CONCURRENTLY IF EXISTS event_kind_idx;
-DROP INDEX CONCURRENTLY IF EXISTS event_attribute_idx;
+DROP INDEX IF EXISTS event_kind_idx;
+DROP INDEX IF EXISTS event_attribute_idx;
