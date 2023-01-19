21f51dd42607dea0298a67a243bfda6fdb4d564a
Dmitry <0nkery@users.noreply.github.com>
2023-01-19 13:31:20 +0100

    Make state and total count queries consistent (#393)

--- src/db/event/set_state.rs
+++ src/db/event/set_state.rs
@@ -147,9 +147,8 @@ impl<'a> Query<'a> {
     pub(crate) async fn total_count(&self, conn: &mut PgConnection) -> sqlx::Result<i64> {
         if let Some(attribute) = self.attribute {
             sqlx::query!(
-                "
-                SELECT COUNT(DISTINCT label) as total FROM (
-                    SELECT
+                "SELECT COUNT(1) as total FROM (
+                    SELECT DISTINCT ON(original_occurred_at, label)
                         *,
                         bool_or(removed) OVER (
                             PARTITION BY room_id, set, label
@@ -176,9 +175,8 @@ impl<'a> Query<'a> {
             .map(|r| r.total.unwrap_or(0))
         } else {
             sqlx::query!(
-                "
-                SELECT COUNT(DISTINCT label) as total FROM (
-                    SELECT
+                "SELECT COUNT(1) as total FROM (
+                    SELECT DISTINCT ON(original_occurred_at, label)
                         *,
                         bool_or(removed) OVER (
                             PARTITION BY room_id, set, label
@@ -316,6 +314,33 @@ mod tests {
         let r = q.clone().execute(&mut conn).await.unwrap();
         assert_eq!(r.len(), 1);
         assert_eq!(r[0].label(), Some(&msg2_label[..]));
+
+        let r = q.total_count(&mut conn).await.unwrap();
+        assert_eq!(r, 1);
+    }
+
+    #[tokio::test]
+    async fn query_with_duplicated_labels() {
+        let db = TestDb::new().await;
+        let agent = TestAgent::new("web", "user123", USR_AUDIENCE);
+
+        let mut conn = db.get_conn().await;
+        let room = shared_helpers::insert_room(&mut conn).await;
+
+        let msg_label = format!("message-{}", Uuid::new_v4());
+
+        message_builder(&room, &msg_label, 1000, &agent, false)
+            .insert(&mut conn)
+            .await;
+
+        message_builder(&room, &msg_label, 7000, &agent, false)
+            .insert(&mut conn)
+            .await;
+
+        let q = Query::new(room.id(), "messages".into(), 100_000, 2);
+
+        let r = q.clone().execute(&mut conn).await.unwrap();
+        assert_eq!(r.len(), 1);
 
         let r = q.total_count(&mut conn).await.unwrap();
         assert_eq!(r, 1);
