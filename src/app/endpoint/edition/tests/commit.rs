75d7e1ff49d33ce97743e7b407591cd59c33bb86
Максим Золотухин <m.v.zolotukhin@foxford.ru>
2023-04-24 13:50:07 +0300

    removal test case

--- src/app/endpoint/edition/tests/commit.rs
+++ src/app/endpoint/edition/tests/commit.rs
@@ -11,7 +11,105 @@ use crate::test_helpers::prelude::*;
 use super::super::*;
 
 #[tokio::test]
-async fn create_bulk_removal_change() {
+async fn removal() {
+    let db = TestDb::new().await;
+    let agent = TestAgent::new("web", "user123", USR_AUDIENCE);
+
+    let (room, edition, events) = {
+        let mut conn = db.get_conn().await;
+        let room = shared_helpers::insert_room(&mut conn).await;
+
+        let edition = shared_helpers::insert_edition(&mut conn, &room, &agent.agent_id()).await;
+
+        let mut events = vec![];
+
+        for i in 1..4 {
+            let event = factory::Event::new()
+                .room_id(room.id())
+                .set("set1")
+                .kind("message")
+                .data(&json!({ "text": format!("message {}", i) }))
+                .occurred_at(i * 1000)
+                .created_by(&agent.agent_id())
+                .insert(&mut conn)
+                .await;
+
+            events.push(event);
+        }
+
+        (room, edition, events)
+    };
+
+    // Allow agent to create editions
+    let mut authz = TestAuthz::new();
+    authz.allow(
+        agent.account_id(),
+        vec!["classrooms", &room.classroom_id().to_string()],
+        "update",
+    );
+
+    // Make edition.create request
+    let mut context = TestContext::new(db.clone(), authz);
+
+    let removed_event = &events[0];
+    let payload = change::CreateRequest {
+        edition_id: edition.id(),
+        changeset: change::Changeset::Removal(change::RemovalData {
+            event_id: removed_event.id(),
+            kind: Some(removed_event.kind().to_string()),
+            set: Some(removed_event.set().to_string()),
+            occurred_at: Some(removed_event.occurred_at()),
+        }),
+    };
+
+    let messages = handle_request::<change::CreateHandler>(&mut context, &agent, payload)
+        .await
+        .expect("Failed to create change");
+
+    // Assert response
+    let (change, respp, _) = find_response::<Change>(messages.as_slice());
+    assert_eq!(respp.status(), ResponseStatus::CREATED);
+    assert_eq!(change.edition_id(), edition.id());
+    assert_eq!(change.kind(), ChangeType::Removal);
+
+    let payload = CommitRequest {
+        id: edition.id(),
+        payload: CommitPayload {
+            offset: 0
+        },
+    };
+
+    let messages = handle_request::<CommitHandler>(&mut context, &agent, payload.clone())
+        .await
+        .expect("Failed to create edition");
+
+    // Assert response
+    let (_, respp, _) = find_response::<serde_json::Value>(messages.as_slice());
+    assert_eq!(respp.status(), ResponseStatus::ACCEPTED);
+
+    let (commit_notification, _, _) = find_event::<EditionCommitNotification>(messages.as_slice());
+    let new_room_id = match commit_notification.result {
+        EditionCommitResult::Error{..} => panic!("error in edition commit notification"),
+        EditionCommitResult::Success { committed_room_id, .. } => committed_room_id,
+    };
+
+    //let mut conn = db.get_conn().await;
+    let mut conn = db.get_conn().await;
+    let events = event::ListQuery::new()
+        .room_id(new_room_id)
+        .execute(&mut conn)
+        .await
+        .expect("Failed to fetch events");
+
+    assert_eq!(events.len(), 2);
+    for ev in events {
+        assert_ne!(ev.id(), removed_event.id());
+    }
+}
+
+
+#[tokio::test]
+async fn bulk_removal() {
     let db = TestDb::new().await;
     let agent = TestAgent::new("web", "user123", USR_AUDIENCE);
 
