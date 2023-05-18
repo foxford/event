89441eba10d6281e1e518de6dbaeb5bcfcda38a6
Максим Золотухин <m.v.zolotukhin@foxford.ru>
2023-05-18 13:54:33 +0300

    code review

--- src/app/error.rs
+++ src/app/error.rs
@@ -182,7 +182,7 @@ impl From<ErrorKind> for ErrorKindProperties {
                 is_notify_sentry: true,
             },
             ErrorKind::S3UploadFailed => ErrorKindProperties {
-                status: ResponseStatus::UNPROCESSABLE_ENTITY,
+                status: ResponseStatus::INTERNAL_SERVER_ERROR,
                 kind: "s3_upload_failed",
                 title: "S3 upload failed",
                 is_notify_sentry: true,
